8f0b24dc5123b1edf589865b1e66d88975e02254
Reggie Cole <reggie@robokind.com>
2019-02-23 20:20:16 -0600

    got the whole kit in finally

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,70 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
-        devtool: 'inline-source-map',
-       devServer: {
-         contentBase: './docs'
-       },
+    entry: {
+        'lib.min': ['@babel/polyfill', 'react', 'react-dom'],
+        'main': ['@babel/polyfill','./src/index.js']
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './docs'
+    },
+    resolve: {
+        alias: {
+            assets: path.resolve(__dirname, 'src/assets/'),
+            views: path.resolve(__dirname, 'src/views/'),
+            components: path.resolve(__dirname, 'src/components/')
+        }
+    },
     plugins: [
         new CleanWebpackPlugin(['docs']),
         new HtmlWebpackPlugin({
-             title: 'Rio Valley Relief Project',
-             template: 'src/index.ejs'
-    }),
+            title: 'Rio Valley Relief Project',
+            template: 'public/index.html'
+        }),
         new CopyWebpackPlugin([
-            { from: 'static', to: 'static' },
+            {from: 'public', to: ''},
         ])
-],
+    ],
     output: {
-    filename: '[name].bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'docs')
     },
     module: {
 
         rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, 'src')],
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        "plugins": [
+                            "@babel/plugin-proposal-class-properties",
+                            "@babel/plugin-transform-modules-commonjs"
+                        ],
+                        "presets": [
+                            "@babel/preset-env",
+                            "@babel/preset-react"]
+                    }
+                }
+
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            },
             {
                 test: /\.css$/,
                 use: [
